7424be20d2b77ed2b33566f64256a772925ab0cd
steadfasterX <steadfastX@boun.cr>
2018-07-17 17:20:49 +0200

    fixes #76
    solving that issue required a complete re-work of the
    welcome screen (buttons instead of field-buttons, FWUL logo,
    wording, ...)
    
    instead of having another annoying popup this issue has been
    implemented as an addition to the regular welcome screen:
    When FWUL detects virtualization it will print out a big red
    warning on the welcome screen pointing to this issue for details
    
    this commit also removes the legacy 32bit hint
    
    Change-Id: Ic0e2d79863bc499a224efe0c600bdeee4d360e18

--- welcome.sh
+++ welcome.sh
@@ -21,8 +21,6 @@
 #
 ######################################################################################################
 
-set -o nounset                              # Treat unset variables as an error
-
 # find and save the path where executed
 EXDIR=${0%/*}
 ICONPATH="$EXDIR/icons"
@@ -32,43 +30,55 @@ CKSPLASH="$HOME/.fwul-splash"
 
 source /etc/fwul-release
 [ -z "$patchlevel" ]&& patchlevel=0
-WELCTXT="<span font='18'>\nWelcome to\n\nFWUL <b>$fwulversion</b></span>\n(build: $fwulbuild, patchlevel: $patchlevel, mode: $fwul_Chassis)\n\n[F]orget [W]indows [U]se [L]inux\n\n"
+WELCTXT="<span font='18'>\n   Welcome to\n\n\tFWUL <b>$fwulversion</b></span>\n\t(build: $fwulbuild, patchlevel: $patchlevel, mode: $fwul_Chassis)\n\n"
 
-ARCH=$(uname -m)
-if [ $ARCH == "i686" ];then
-    WELCTXT="$WELCTXT<span foreground='red'><b>YOU ARE USING 32bit HARDWARE!</b>\nKeep in mind that FWUL provides limited support for 32bit only\n(e.g. some tools like JOdin are missing here)</span>\n\n"
+if [ "$fwul_Chassis" == "vm" ];then
+        WELCTXT="$WELCTXT\t<span font='14' foreground='red'><b>!! Virtualization detected !!</b></span><span foreground='red'>\n\n\twhen doing critical operations (like flashing a ROM/bootloader) do <b><u>not</u> use FWUL from within VirtualBox.</b>\n\n\t(This is not a limitation of FWUL but is generally related to virtualization - <a href='https://github.com/Carbon-Fusion/build_fwul/issues/76'>click here</a> for details)</span>\n"
+else
+        WELCTXT="$WELCTXT\n\n\n\n\n"
 fi
 
-YRET=$(yad --title "Welcome to FWUL" --undecorated --skip-taskbar --height=650 --center --width=700 \
-    --button="Disable Welcome Screen (for persistent mode only)":11 \
-    --button=Close:99 \
-    --buttons-layout="edge" \
-    --no-escape \
-    --text "$WELCTXT\n- double-click an entry to open -" --text-align=center \
-    --list --grid-lines=hor --no-headers --print-column=3 \
-    --column="icon:IMG" --column="what:TXT" --column="parse:HD" \
-    "$ICONPATH/faq.png" "<b>FAQ</b>" faq\
-    "$ICONPATH/support.png" "<b>Support</b>" support \
-    "$ICONPATH/issues.png" "<b>Bug/Feature Request</b>" issue \
-    "$ICONPATH/website.png" "<b>Project page</b>" web \
-    "$ICONPATH/build.png" "<b>Build FWUL / Sources</b>" sources)
+F_FAQ(){
+        xdg-open 'https://forum.xda-developers.com/showpost.php?p=70272692&postcount=4'
+}; export -f F_FAQ
+F_SUPPORT(){
+       xdg-open "http://webchat.freenode.net/?channels=Carbonfusion-user"
+}; export -f F_SUPPORT
+F_ISSUE(){
+        xdg-open "https://github.com/Carbon-Fusion/build_fwul/issues/new"
+}; export -f F_ISSUE
+F_INSTALL(){
+        yad --title SORRY --center --text '\nSorry this is not ready yet.\nCheck the FWUL Project page for details and process.'
+}; export -f F_INSTALL
+F_WEB(){
+        xdg-open "https://tinyurl.com/FWULatXDA"
+}; export -f F_WEB
+F_SOURCES(){
+        xdg-open "https://github.com/Carbon-Fusion/build_fwul"
+}; export -f F_SOURCES
+
+F_CLOSE(){
+        kill -s SIGUSR1 $YAD_PID >> /dev/null 2>&1
+}; export -f F_CLOSE
+
+YRET=$(yad -on-top --no-escape --title="Welcome to FWUL" --width=1000 --image="$ICONPATH/welcome.png" --undecorated --skip-taskbar --center --always-print-result --height=400\
+        --text "$WELCTXT\n\n" \
+        --form --field="":LBL true --field=" Disable Welcome Screen (only for persistent build)":CHK false \
+        --buttons-layout=spread \
+        --button="Close":"bash -c F_CLOSE"\
+        --button="FAQ!$ICONPATH/faq.png!Frequently Asked Questions":"bash -c F_FAQ" \
+        --button="Support!$ICONPATH/support.png!Get immediate support on IRC":"bash -c F_SUPPORT" \
+        --button="Bugs!$ICONPATH/issues.png!File a bug / Request a new feature":"bash -c F_ISSUE" \
+        --button="Forum!$ICONPATH/website.png!Forum / Homepage":"bash -c F_WEB" \
+        --button="Sources!$ICONPATH/build.png!Build your own version of FWUL":"bash -c F_SOURCES" \
+        )
+# parse selection
+SELECT=$(echo "${YRET}"|cut -d "|" -f2)
 
 # disable welcome screen on next boot
-if [ $? -eq 11 ];then
+if [ "$SELECT" == "TRUE" ];then
     date > $CKSPLASH
     exit
 fi
 
-# parse selection
-SELECT="${YRET/|/}"
 
-# act based on selection
-case "$SELECT" in
-    faq) xdg-open "https://forum.xda-developers.com/showpost.php?p=70272692&postcount=4" ;;
-    support) xdg-open "http://webchat.freenode.net/?channels=Carbon-user" ;;
-    issue) xdg-open "https://github.com/Carbon-Fusion/build_fwul/issues/new" ;;
-    install) yad --title SORRY --center --text '\nSorry this is not ready yet.\nCheck the FWUL Project page for details and process.' ;;
-    web) xdg-open "https://tinyurl.com/FWULatXDA";;
-    sources) xdg-open "https://github.com/Carbon-Fusion/build_fwul";;
-    *)  echo "ERROR: $YRET";;
-esac
